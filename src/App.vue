b12941e241c93d05d1817dc4b85567c5e24f30a2
Hong Kyuyoung <hky0715@gmail.com>
2024-06-28 18:08:03 +0900

    Feat: 스터디룸 예약화면 생성

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <main>
+      <router-view></router-view>
+    </main>
 </template>
 
 <style scoped>
---
+++ src/components/Main.vue
@@ -0,0 +1,58 @@
+<script setup>
+  import { onMounted, ref } from 'vue';
+  import axios from 'axios';
+
+  const seats = ref([]);
+
+
+  function checkIn() {
+    alert('좌석 예약이 완료되었습니다.');
+  }
+  function checkOut() {
+    alert('퇴실 처리 되었습니다.');
+  }
+</script>
+
+<template>
+    <section class="section-layout">
+    <h1 style="display: none">스터디카페명</h1>
+      <div>
+        <h1>TUNA STUDY CAFE</h1>
+      </div>
+    </section>
+    <section class="section-layout">
+      <h1 style="display: none">전화번호 입력폼</h1>
+      <div class="div-layout">
+        <input type="text" style="flex-grow: 1" placeholder="전화번호 입력박스"/>
+        <button class="small-button">조회</button>
+      </div>
+    </section>
+    <section class="section-layout" style="justify-content: space-between">
+      <h1 style="display: none">좌석버튼</h1>
+      <div>
+        <div class="div-layout" style="margin-bottom: 20px;">
+          <button class="seat-button">01</button>
+          <button class="seat-button">02</button>
+          <button class="seat-button selected-seat">03</button>
+          <button class="seat-button unable-seat">04</button>
+        </div>
+        <div class="div-layout">
+          <button class="seat-button unable-seat">05</button>
+          <button class="seat-button">06</button>
+          <button class="seat-button">07</button>
+          <button class="seat-button">08</button>
+        </div>
+      </div>
+    </section>
+    <section class="section-layout">
+      <h1 style="display: none">자리예약/퇴실</h1>
+      <div class="div-layout">
+        <button class="large-button" @click="checkIn()">자리예약</button>
+        <button class="large-button" @click="checkOut()">퇴실</button>
+      </div>
+    </section>
+</template>
+
+<style scoped>
+
+</style>
